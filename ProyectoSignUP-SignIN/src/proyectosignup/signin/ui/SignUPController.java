d0dafaab07130b2684273ec330d17e05e2c4a6b2
Chad <chad154154@gmail.com>
2025-10-24 13:28:01 +0200

    Added methods about the perfomance of the window

--- ProyectoSignUP-SignIN/src/proyectosignup/signin/ui/SignUPController.java
+++ ProyectoSignUP-SignIN/src/proyectosignup/signin/ui/SignUPController.java
@@ -1,22 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectosignup.signin.ui;
 
 import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/**
- *
- * @author chad
- */
 public class SignUPController {
 
     @FXML
@@ -45,9 +38,11 @@ public class SignUPController {
     private Button btSignUp;
     @FXML
     private Button btExit;
+
     private static final Logger LOGGER = Logger.getLogger("proyectosignup.signin.ui");
 
     public void init(Stage stage) {
+        try{
         /*
         Establecer titulo de la ventana
         La ventana mostrará todos los campos necesarios para el registro de un nuevo usuario.
@@ -60,7 +55,70 @@ public class SignUPController {
         stage.setTitle("Sign UP");
         stage.setResizable(false);
 
+        btSignUp.setDisable(true);
+        btExit.setDisable(false);
+        
+
+        // Los campos de username y password tendrán un texto de fondo.
+        // Se enfoca inicialmente en el campo username.
+        // asociar eventos a manejadores
+        btExit.setOnAction(this::handlebExitMethod);
+        btSignUp.setOnAction(this::handlebLogInMethod);
+
+        // asociacion de manejadores a properties
+        tfName.textProperty().addListener(this::handletfUsernameTextChange);
+        tfName.focusedProperty().addListener(this::handletfUsernameFocusChange);
+
+        pwPaswword.textProperty().addListener(this::handleftPasswordTextChange);
+        pwPaswword.focusedProperty().addListener(this::handletfPasswordFocusChange);
+        }catch (Exception e){
+            String errorMSG="Error abriendo la ventana:\n" + e.getMessage();
+        }
     }
 
-}
+    // botones -------------------
+    private void handlebExitMethod(ActionEvent event) {
+        Platform.exit();
+    }
 
+    private void handlebLogInMethod(ActionEvent event) {
+        // acción del botón SignUp
+        try{
+            if(tfName!=null)
+                throw new Exception ("El nombre no puede estar vacio");
+            if(tfLastname!=null)
+                throw new Exception ("El apellido no puede estar vacio");
+            if(tfMidleeInitial!=null)
+                throw new Exception ("No puede estar vacio");
+            if(tfStreet!=null)
+                throw new Exception ("La calle no puede estar vacio");
+            if(tfState!=null)
+                throw new Exception ("El estado no puede estar vacio");
+             if(tfZip!=null)
+                throw new Exception ("El Zip no puede estar vacio");
+        }catch(Exception e){
+            
+        }
+    }
+
+    // campos --------------------
+    private void handletfUsernameTextChange(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        // aquí manejar cambio de texto
+    }
+
+    private void handletfUsernameFocusChange(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+        if (!newValue) {
+            // si se pierde el foco
+        }
+    }
+
+    private void handleftPasswordTextChange(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        // cambio en contraseña
+    }
+
+    private void handletfPasswordFocusChange(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+        if (!newValue) {
+            // si se pierde el foco
+        }
+    }
+}
