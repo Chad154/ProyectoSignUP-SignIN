fec701638f1deac79df6a327d975a2e24aa75bea
Chad <chad154154@gmail.com>
2025-10-30 13:06:30 +0100

    Añadido validaciones para el Sign Up (Unicamente que no este vacio), ademas se ha añadido el codigo para crear el nuevo Customer para la base de datos y mandarselo al RestFull

--- ProyectoSignUP-SignIN/src/proyectosignup/signin/ui/SignUPController.java
+++ ProyectoSignUP-SignIN/src/proyectosignup/signin/ui/SignUPController.java
@@ -5,10 +5,14 @@ import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import proyectosignup.signin.logic.CustomerRESTClient;
+import proyectosignup.signin.model.Customer;
 
 public class SignUPController {
 
@@ -31,7 +35,7 @@ public class SignUPController {
     @FXML
     private TextField tfEmail;
     @FXML
-    private PasswordField pwPaswword;
+    private PasswordField pwPassword;
     @FXML
     private PasswordField pfPasswordValidation;
     @FXML
@@ -42,37 +46,36 @@ public class SignUPController {
     private static final Logger LOGGER = Logger.getLogger("proyectosignup.signin.ui");
 
     public void init(Stage stage) {
-        try{
-        /*
+        try {
+            /*
         Establecer titulo de la ventana
         La ventana mostrará todos los campos necesarios para el registro de un nuevo usuario.
         El foco se establecerá en el campo Name.
         El botón Sign Up estará deshabilitado hasta que todos los campos requeridos estén informados.
         El botón Exit estará habilitado.
         El título “Sign Up” se mostrará centrado en la parte superior
-         */
-        LOGGER.info("Initialize Window");
-        stage.setTitle("Sign UP");
-        stage.setResizable(false);
+             */
+            LOGGER.info("Initialize Window");
+            stage.setTitle("Sign UP");
+            stage.setResizable(false);
 
-        btSignUp.setDisable(true);
-        btExit.setDisable(false);
-        
+            btSignUp.setDisable(true);
+            btExit.setDisable(false);
 
-        // Los campos de username y password tendrán un texto de fondo.
-        // Se enfoca inicialmente en el campo username.
-        // asociar eventos a manejadores
-        btExit.setOnAction(this::handlebExitMethod);
-        btSignUp.setOnAction(this::handlebLogInMethod);
+            // Los campos de username y password tendrán un texto de fondo.
+            // Se enfoca inicialmente en el campo username.
+            // asociar eventos a manejadores
+            btExit.setOnAction(this::handlebExitMethod);
+            btSignUp.setOnAction(this::handlebLogInMethod);
 
-        // asociacion de manejadores a properties
-        tfName.textProperty().addListener(this::handletfUsernameTextChange);
-        tfName.focusedProperty().addListener(this::handletfUsernameFocusChange);
+            // asociacion de manejadores a properties
+            tfName.textProperty().addListener(this::handletfUsernameTextChange);
+            tfName.focusedProperty().addListener(this::handletfUsernameFocusChange);
 
-        pwPaswword.textProperty().addListener(this::handleftPasswordTextChange);
-        pwPaswword.focusedProperty().addListener(this::handletfPasswordFocusChange);
-        }catch (Exception e){
-            String errorMSG="Error abriendo la ventana:\n" + e.getMessage();
+            pwPassword.textProperty().addListener(this::handleftPasswordTextChange);
+            pwPassword.focusedProperty().addListener(this::handletfPasswordFocusChange);
+        } catch (Exception e) {
+            String errorMSG = "Error abriendo la ventana:\n" + e.getMessage();
         }
     }
 
@@ -83,21 +86,60 @@ public class SignUPController {
 
     private void handlebLogInMethod(ActionEvent event) {
         // acción del botón SignUp
-        try{
-            if(tfName!=null)
-                throw new Exception ("El nombre no puede estar vacio");
-            if(tfLastname!=null)
-                throw new Exception ("El apellido no puede estar vacio");
-            if(tfMidleeInitial!=null)
-                throw new Exception ("No puede estar vacio");
-            if(tfStreet!=null)
-                throw new Exception ("La calle no puede estar vacio");
-            if(tfState!=null)
-                throw new Exception ("El estado no puede estar vacio");
-             if(tfZip!=null)
-                throw new Exception ("El Zip no puede estar vacio");
-        }catch(Exception e){
-            
+        try {
+            //Se realizan las validaciones de todos los campos para que no esten vacios
+            if (this.tfName.getText().trim().equals("")
+                    || this.tfLastname.getText().trim().equals("")
+                    || this.tfMidleeInitial.getText().trim().equals("")
+                    || this.tfStreet.getText().trim().equals("")
+                    || this.tfCity.getText().trim().equals("")
+                    || this.tfState.getText().trim().equals("")
+                    || this.tfZip.getText().trim().equals("")
+                    || this.tfPhone.getText().trim().equals("")
+                    || this.tfEmail.getText().trim().equals("")
+                    || this.pwPassword.getText().trim().equals("")
+                    || this.pfPasswordValidation.getText().trim().equals("")) {
+                Alert alert = new Alert(AlertType.ERROR, "Los campos no pueden estar vacios");
+            } else {
+                //Creamos el objeto RestClient
+                CustomerRESTClient client = new CustomerRESTClient();
+                //Creaamos la variable ultimo ID inicializado en 0
+                int lastId = 0;
+                //Creamos un booleano "Encontrado" en true
+                boolean found = true;
+                //Realizamos un bucle utilizando el found para encontrar el ultimo ID existente
+                //El bucle pide el Id mediante el find_XML sumandole 1 hasta que el propio metodo nos de un 404 donde ahi ha encontrado el ultimo ID, el cual cogemos.
+                while (found) {
+                    try {
+                        client.find_XML(Customer.class, String.valueOf(lastId + 1));
+                        lastId++;
+                    } catch (Exception e) {
+                        found = false;
+                    }
+
+                }
+                //Al encontrar el ultimo ID 
+                Long newId = Long.valueOf(lastId + 1);
+
+                Customer customer = new Customer();
+                customer.setId(newId);
+                customer.setFirstName(tfName.getText().trim());
+                customer.setLastName(tfLastname.getText().trim());
+                customer.setMiddleInitial(tfMidleeInitial.getText().trim());
+                customer.setStreet(tfStreet.getText().trim());
+                customer.setCity(tfCity.getText().trim());
+                customer.setState(tfState.getText().trim());
+                customer.setZip(Integer.parseInt(tfZip.getText().trim()));
+                customer.setPhone(Long.parseLong(tfPhone.getText().trim()));
+                customer.setEmail(tfEmail.getText().trim());
+                customer.setPassword(pwPassword.getText().trim());
+
+                client.create_XML(customer);
+                client.close();
+
+            }
+        } catch (Exception e) {
+
         }
     }
 
